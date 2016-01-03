aa4780e2793b19133ba9c2f361d144506e1727be
git_user <admin@tradom.ca>
2016-01-03 09:56:18 -0500

    test unitaire formulaires

--- univ/tests/test_formulaires.py
+++ univ/tests/test_formulaires.py
@@ -1,6 +1,21 @@
 from django.test import TestCase
+from univ.forms import InscriptionForm, ListeCoursForm, ListeProgrammeForm
 
 
 class FormulaireTestCase(TestCase):
-    pass
+
+    def setUp(self):
+        ins = InscriptionForm()
+        cours = ListeCoursForm()
+        prog = ListeProgrammeForm()
+
+    def test_inscriptionform(self):
+        pass
+
+    def test_listecoursform(self):
+        pass
+
+    def test_listeprogrammeform(self):
+        pass
+
 
